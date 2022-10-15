93e08f272bcdd7e0654d10080d9e5fcfadc77eec
sumeet321 <coolsoccer6@gmail.com>
2022-10-15 10:59:31 -0700

    High Score and shooting
    High Score, shooting.

---
+++ Scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    public Text highScoreText;
+    private float score;
+    public float highScore;
+   
+    void Start()
+    {
+        highScore = PlayerPrefs.GetFloat("Highscore");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            score += 50 * Time.deltaTime;
+            scoreText.text = ((int)score).ToString();
+        }
+        highScoreText.text = highScore.ToString();
+        if(score > highScore)
+        {
+            PlayerPrefs.SetFloat("Highscore", Mathf.FloorToInt(score));
+        }
+    }
+}
---
+++ Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //Shooting logic
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
