98c7c44ae9ae6a3fdef00b0393c41bb0b98f2d4d
Khang Truong <truonggiakhang99@gmail.com>
2022-10-15 15:53:26 -0700

    PauseMenu v2

--- Scripts/Weapon.cs
+++ Scripts/Weapon.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public bool callToPause;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +16,8 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        callToPause = PauseMenu.gameIsPause;
+        if (Input.GetButtonDown("Fire1") && callToPause != true)
         {
             Shoot();
         }
